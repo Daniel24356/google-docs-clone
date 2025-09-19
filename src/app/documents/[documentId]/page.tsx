d851b531ac8553a4ab20b6048f1010dff2721aaf
Daniel24356 <igwedaniel128@gmail.com>
2025-09-19 10:59:11 -0700

    Show document creation date and owner

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,36 +1,36 @@
-// import { Id } from "../../../../convex/_generated/dataModel";
-// import { Document } from "@/app/documents/[documentId]/document";
-// import { preloadQuery } from "convex/nextjs";
-// import { auth } from "@clerk/nextjs/server";
-// import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { Document } from "@/app/documents/[documentId]/document";
+import { preloadQuery } from "convex/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { api } from "../../../../convex/_generated/api";
 
-// interface DocumentIdPageProps {
-//   params: Promise<{ documentId: Id<"documents"> }>;
-// }
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: Id<"documents"> }>;
+}
 
-// const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
-//   const { documentId } = await params;
+const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
+  const { documentId } = await params;
 
-//   const { getToken } = await auth();
-//   const token = (await getToken({ template: "convex" })) ?? undefined;
+  const { getToken } = await auth();
+  const token = (await getToken({ template: "convex" })) ?? undefined;
 
-//   if (!token) {
-//     console.error("Unauthorized access attempt");
-//     throw Error("Unauthorized");
-//   }
+  if (!token) {
+    console.error("Unauthorized access attempt");
+    throw Error("Unauthorized");
+  }
 
-//   const preloadedDocument = await preloadQuery(
-//     api.documents.getById,
-//     { id: documentId },
-//     { token }
-//   );
+  const preloadedDocument = await preloadQuery(
+    api.documents.getById,
+    { id: documentId },
+    { token }
+  );
 
-//   if (!preloadedDocument) {
-//     console.error("Document not found:", documentId);
-//     return <div>Document not found</div>;
-//   }
+  if (!preloadedDocument) {
+    console.error("Document not found:", documentId);
+    return <div>Document not found</div>;
+  }
 
-//   return <Document preloadedDocument={preloadedDocument} />;
-// };
+  return <Document preloadedDocument={preloadedDocument} />;
+};
 
-// export default DocumentIdPage;
+export default DocumentIdPage;
